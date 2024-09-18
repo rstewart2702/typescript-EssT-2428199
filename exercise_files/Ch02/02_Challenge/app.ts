0b3e66ca7662478adeea712306a7e4f274ce0d47
Richard S. Stewart <rstewart2702@gmail.com>
2024-09-18 15:48:38 -0500

    Initial instructor directions for the exercise.

--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -1,8 +1,17 @@
+// Types exercise:
+
+// Create an interface to describe the structure of the
+// individual todoItems.
 const todoItems = [
     { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
     { id: 2, title: "Learn TypeScript", status: "in-progress" },
     { id: 3, title: "Write the best app in the world", status: "todo" },
 ]
+
+// strongly-type the hard-coded values with an "enum"
+// for the object field/attribute named "status"
+
+// apply types to the parameters and return values of the functions.
 
 function addTodoItem(todo) {
     const id = getNextId(todoItems)
@@ -17,6 +26,9 @@ function addTodoItem(todo) {
 
     return newTodo
 }
+
+// Use a generic type (i.e., a type parameter) for the argument type
+// and return type of the genNextId function:
 
 function getNextId(items) {
     return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1
